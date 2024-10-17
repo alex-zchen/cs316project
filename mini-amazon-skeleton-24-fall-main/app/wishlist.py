9ff2ff5b4683944c139a318a0338c39c90bc2941
Joseph Wang <jhw45@duke.edu>
2024-10-17 16:25:22 -0400

    wishlist.html finished

--- mini-amazon-skeleton-24-fall-main/app/wishlist.py
+++ mini-amazon-skeleton-24-fall-main/app/wishlist.py
@@ -1,9 +1,15 @@
 from flask import jsonify
+from flask import render_template, request
 from flask import redirect, url_for
 from flask_login import current_user
 import datetime
 
 from .models.wishlist import WishlistItem
+
+from humanize import naturaltime
+
+def humanize_time(dt):
+    return naturaltime(datetime.datetime.now() - dt)
 
 from flask import Blueprint
 bp = Blueprint('wishlist', __name__)
@@ -15,7 +21,9 @@ def wishlist():
     if current_user.is_authenticated:
         items = WishlistItem.get_all_by_uid_since(
             current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
-        return jsonify([item.__dict__ for item in items])
+        return render_template('wishlist.html',
+                       items=items,
+                       humanize_time=humanize_time)
     else:
         return jsonfiy({}), 404
 
